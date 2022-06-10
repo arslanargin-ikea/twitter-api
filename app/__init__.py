bb10ff88e23b51bc7bc233dc115418115eb03429
Ayse Arslanargin Dereli <ayse.arslanargin.dereli@ingka.ikea.com>
2022-06-10 11:45:44 +0200

    updated the api

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    
+    from config import Config
+    from app.models import Tweet
+    from flask_migrate import Migrate
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate = Migrate(app, db)
 
     from .apis.tweets import api as tweets
     api = Api()
