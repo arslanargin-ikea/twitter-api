2296ccd24fb44dd4f5e522d741a2e06a934a6153
Ayse Arslanargin Dereli <ayse.arslanargin.dereli@ingka.ikea.com>
2022-06-10 13:34:27 +0200

    added endpoints

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -12,6 +12,10 @@ json_tweet = api.model('Tweet', {
 })
 
 
+json_new_tweet = api.model('New tweet', {
+    'text': fields.String(required=True, min_length=1),  # Don't allow empty string
+})
+
 @api.route('/<int:id>')
 @api.response(404, 'Tweet not found')
 @api.param('id', 'The tweet unique identifier')
@@ -23,3 +27,42 @@ class TweetResource(Resource):
             api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
             return tweet
+        
+    @api.marshal_with(json_tweet, code=200)
+    @api.expect(json_new_tweet, validate=True)
+    def patch(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            tweet.text = api.payload["text"]
+            db.session.commit()
+            return tweet
+        
+    def delete(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            db.session.delete(tweet)
+            db.session.commit()
+            return None
+   
+@api.route('')
+@api.response(422, 'Invalid tweet')
+class TweetsResource(Resource):
+    @api.marshal_with(json_tweet, code=201)
+    @api.expect(json_new_tweet, validate=True)
+    def post(self):
+        text = api.payload["text"]
+        if len(text) > 0:
+            tweet = Tweet(text=text)
+            db.session.add(tweet)
+            db.session.commit()
+            return tweet, 201
+        else:
+            return abort(422, "Tweet text can't be empty")
+
+    @api.marshal_with(json_tweet)
+    def get(self):
+        return db.session.query(Tweet).all(), 201
--- tests/apis/test_tweet_view.py
+++ tests/apis/test_tweet_view.py
@@ -26,3 +26,27 @@ class TestTweetViews(TestCase):
         self.assertEqual(response_tweet["id"], 1)
         self.assertEqual(response_tweet["text"], "First tweet")
         self.assertIsNotNone(response_tweet["created_at"])
+        
+    def test_tweet_create(self):
+        response = self.client.post("/tweets", json={'text': 'New tweet!'})
+        created_tweet = response.json
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(created_tweet["id"], 1)
+        self.assertEqual(created_tweet["text"], "New tweet!")
+
+    def test_tweet_update(self):
+        first_tweet = Tweet(text="First tweet")
+        db.session.add(first_tweet)
+        db.session.commit()
+        response = self.client.patch("/tweets/1", json={'text': 'New text'})
+        updated_tweet = response.json
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(updated_tweet["id"], 1)
+        self.assertEqual(updated_tweet["text"], "New text")
+
+    def test_tweet_delete(self):
+        first_tweet = Tweet(text="First tweet")
+        db.session.add(first_tweet)
+        db.session.commit()
+        self.client.delete("/tweets/1")
+        self.assertIsNone(db.session.query(Tweet).get(1))
